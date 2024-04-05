9258385517fcf1a7d924f6e3b2eb6fe7ba3acd76
Matias Mendoza <mendoza.e.matias@gmail.com>
2024-04-05 19:17:03 -0300

    delete coments

--- tp-uno/primertp.py
+++ tp-uno/primertp.py
@@ -1,3 +1,18 @@
+# ### **Ejercicio 19**
+
+# Las y los alumnas y alumnos de un curso se han dividido en dos grupos A y B de acuerdo al género y el nombre. El grupo A esta formado por las personas de genero femenino con una inicial de nombre anterior a la M y las personas de genero masculino con un inicial de nombre posterior a la N y el grupo B por el resto. Escribir un programa que pregunte la inicial y el género, y muestre por pantalla el grupo que corresponde.
+
+inicial = input("Ingrese la inicial del nombre")
+genero = input("Ingrese el genero - M o F")
+
+# #Grupo A esta definido F y inicial anterior < M y inicial > N
+# #Grupo B esta definido M y inicial anterior
+
+if genero == 'F' and inicial < 'M' or genero == 'M' and inicial > 'N':
+    print("Grupo A")
+else:
+    print("Grupo B")
+    
 ### **Ejercicio 25**
 #  Escribir un programa que pida un número entero y 
 #  muestre por pantalla un triángulo rectángulo 
@@ -8,10 +23,10 @@
 #  ***
 #  ****
 
-# number = int(input("Ingresa un numero"))
+number = int(input("Ingresa un numero"))
 
-# for i in range(number+1):
-#    print("*"*i)
+for i in range(number+1):
+   print("*"*i)
 
 #------------------------------------------------------------------------------------------------------
 
@@ -22,15 +37,15 @@
 
 # 1 + 2 + 3 + 4 + 5 = 15
    
-# count = 1;
-# two_number = int(input("Ingresa un numero"))
-# response = 0
+count = 1;
+two_number = int(input("Ingresa un numero"))
+response = 0
 
-# while(count < two_number+1):
-#     response += count
-#     count+=1
+while(count < two_number+1):
+    response += count
+    count+=1
 
-# print(response)
+print(response)
 
 #------------------------------------------------------------------------------------------------------
 
@@ -43,14 +58,14 @@
 # -  ......
 # -  Para cualquier N -> Resultado = 2 + 4 + 6 +....+ 2*N
 
-# three_number = int(input("Ingrese un numero"))
-# two_response = 0
+three_number = int(input("Ingrese un numero"))
+two_response = 0
 
-# for indice in range(1,three_number*2 + 1):
-#     if(indice % 2 == 0):
-#         two_response += indice
+for indice in range(1,three_number*2 + 1):
+    if(indice % 2 == 0):
+        two_response += indice
 
-# print(two_response)
+print(two_response)
 
 #------------------------------------------------------------------------------------------------------
 
@@ -68,12 +83,20 @@
 
 # 9 7 5 3 1 
 
-# fourt_number = int(input("Ingresa un numero"))
+fourt_number = int(input("Ingresa un numero"))
 
-# for indice in range(1,fourt_number+1): #Si mi fourt_number = 6 indice = 6
-#     for column in reversed(range(1,indice*2,2)): #Paso ese valor y lo multiplico 6*2 = 1 3 5 7 9 11 (salto de impares)
-#         print(str(column),end=" ")
-#     print("")
+for indice in range(1,fourt_number+1): #Si mi fourt_number = 6 indice = 6
+    for column in reversed(range(1,2*indice,2)): #Paso ese valor y lo multiplico 6*2 = 1 3 5 7 9 11 (salto de impares)
+        print(str(column),end=" ")
+    print("")
+
+
+N = 5
+linea = ''
+
+for i in range(1,2*N,2):
+    linea = str(i) + " " + linea
+    print(linea)
 
 #------------------------------------------------------------------------------------------------------
 
@@ -95,11 +118,11 @@
 
 # 6 | 6
 
-five_number = int(input("Ingrese un numero"))
+number = int(input("Ingrese un numero"))
 
-for indice in range(five_number): #segundo valor en el parentesis
-    for two_indice in range(five_number):
-        print("|" + str(two_indice) + "-" + str(indice) + "|",end=" ")
+for i in range(number): #segundo valor en el parentesis
+    for j in range(i,number):
+        print("|" + str(i) + "-" + str(j) + "|",end=" ")
     print()
 
 #------------------------------------------------------------------------------------------------------
